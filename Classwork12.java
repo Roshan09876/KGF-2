4243ea777a18ff634f18d531ac09a9ca46d81d79
Roshan09876 <94774694+Roshan09876@users.noreply.github.com>
2022-04-26 14:39:56 +0545

    c

--- Classwork12.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-//Write a JAVA program to check whether a year is leap year or not.
-public class Classwork12 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-    }
-}
